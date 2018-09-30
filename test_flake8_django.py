d28d03d3fdb6628ccaf9cf8cf585f628f0bac246
Rocio Aramberri <rocioaramberri@schegel.net>
2018-09-30 15:20:57 -0300

    Add more tests and coverage report

--- test_flake8_django.py
+++ test_flake8_django.py
@@ -12,30 +12,58 @@ def run_check(code):
 
 
 @pytest.mark.parametrize('field_type', NOT_NULL_TRUE_FIELDS)
-def test_not_null_fields(field_type):
+def test_not_null_fields_fails(field_type):
     code = "field = models.{}(null=True)".format(field_type)
     assert len(run_check(code)) == 1
+    assert 'DJ01' in run_check(code)[0][2]
+
+
+@pytest.mark.parametrize('field_type', NOT_NULL_TRUE_FIELDS)
+def test_not_null_fields_success(field_type):
+    code = "field = models.{}()".format(field_type)
+    assert len(run_check(code)) == 0
 
 
 @pytest.mark.parametrize('field_type', NOT_BLANK_TRUE_FIELDS)
-def test_not_blank_fields(field_type):
+def test_not_blank_fields_fails(field_type):
     code = "another_field = models.{}(blank=True)".format(field_type)
     assert len(run_check(code)) == 1
+    assert 'DJ02' in run_check(code)[0][2]
 
 
-def test_booleanfield_blank_and_null():
+@pytest.mark.parametrize('field_type', NOT_BLANK_TRUE_FIELDS)
+def test_not_blank_fields_sucess(field_type):
+    code = "another_field = models.{}()".format(field_type)
+    assert len(run_check(code)) == 0
+
+
+def test_booleanfield_blank_and_null_fails():
     code = "field = models.BooleanField(blank=True, null=True)"
     assert len(run_check(code)) == 2
+    assert 'DJ01' in run_check(code)[1][2]
+    assert 'DJ02' in run_check(code)[0][2]
 
 
-def test_url_name_with_dash():
+def test_url_name_with_dash_fails():
     code = "url(name='test-1')"
     assert len(run_check(code)) == 1
+    assert 'DJ03' in run_check(code)[0][2]
 
 
-def test_render_doesnt_use_locals():
+def test_url_name_with_dash_sucess():
+    code = "url(name='test_1')"
+    assert len(run_check(code)) == 0
+
+
+def test_render_doesnt_use_locals_fails():
     code = "render(request, 'template.html', locals())"
     assert len(run_check(code)) == 1
+    assert 'DJ04' in run_check(code)[0][2]
+
+
+def test_render_doesnt_use_locals_success():
+    code = "render(request, 'template.html', {'test': 'test'})"
+    assert len(run_check(code)) == 0
 
 
 def test_url_include_without_namespace_fails():
