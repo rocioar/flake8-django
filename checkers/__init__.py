231ab1718ef4f6f2dc6df61127b4b324e72446be
Rodolfo Lottin Pereira <rodolfolottin1@gmail.com>
2018-10-14 17:31:29 -0300

    Add checker DJ08 for models that inherits from django models and do not override __str__ method

--- checkers/__init__.py
+++ checkers/__init__.py
@@ -1,7 +1,8 @@
+from .model_dunder_str import ModelDunderStrMissingChecker
 from .model_fields import ModelFieldChecker
 from .model_form import ModelFormChecker
 from .render import RenderChecker
 from .urls import URLChecker
 
 
-__all__ = ['ModelFieldChecker', 'ModelFormChecker', 'RenderChecker', 'URLChecker']
+__all__ = ['ModelDunderStrMissingChecker', 'ModelFieldChecker', 'ModelFormChecker', 'RenderChecker', 'URLChecker']
