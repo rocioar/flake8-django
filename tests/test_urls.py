2150ad7b716210030766ca5b7beb4aee8478d2d1
Rocio Aramberri <rocioaramberri@schegel.net>
2019-03-08 10:56:41 -0300

    Include check for path and re_path on urls checker

--- tests/test_urls.py
+++ tests/test_urls.py
@@ -3,26 +3,42 @@ import pytest
 from .utils import run_check
 
 
-def test_url_name_with_dash_fails():
-    code = "url(name='test-1')"
+@pytest.mark.parametrize('code', [
+    "url(name='test-1')",
+    "path(name='test-1')",
+    "re_path(name='test-1')",
+])
+def test_url_name_with_dash_fails(code):
     assert len(run_check(code)) == 1
     assert 'DJ04' in run_check(code)[0][2]
 
 
-def test_url_name_with_underscore_sucess():
-    code = "url('/test/', View.as_view(), name='test_1')"
+@pytest.mark.parametrize('code', [
+    "url('/test/', View.as_view(), name='test_1')",
+    "path('/test/', View.as_view(), name='test_1')",
+    "re_path('/test/', View.as_view(), name='test_1')",
+])
+def test_url_name_with_underscore_sucess(code):
     assert len(run_check(code)) == 0
 
 
-def test_url_include_without_namespace_fails():
-    code = "url('/test/', include())"
+@pytest.mark.parametrize('code', [
+    "url('/test/', include())",
+    "path('/test/', include())",
+    "re_path('/test/', include())"
+])
+def test_url_include_without_namespace_fails(code):
     assert len(run_check(code)) == 1
     assert 'DJ05' in run_check(code)[0][2]
 
 
 @pytest.mark.parametrize('code', [
     "url('/test/', include('test', namespace='test'))",
-    "url('/test/', includes('test2', namespace='test-2'))"
+    "url('/test/', includes('test2', namespace='test-2'))",
+    "path('/test/', include('test', namespace='test'))",
+    "path('/test/', includes('test2', namespace='test-2'))",
+    "re_path('/test/', include('test', namespace='test'))",
+    "re_path('/test/', includes('test2', namespace='test-2'))",
 ])
 def test_url_include_with_namespace_success(code):
     assert len(run_check(code)) == 0
