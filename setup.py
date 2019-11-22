686cba58ec4d338081c6d3c033795a31bdc53609
Vaclav Rehak <vaclav@verotel.cz>
2019-11-22 09:06:08 +0100

    Revert unintended change in setup.py

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
         ],
     },
     install_requires=['flake8'],
-    tests_require=['pytest','pytest-cov'],
+    tests_require=['pytest'],
     classifiers=[
         'Framework :: Flake8',
         'License :: OSI Approved :: MIT License',
