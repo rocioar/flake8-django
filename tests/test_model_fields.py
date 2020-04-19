b40d21dfec6d6df7d710b60b798b6b92fd9cafce
Rocio Aramberri <rocioaramberri@schegel.net>
2020-04-19 10:37:38 -0300

    Removes DJ02 check

--- tests/test_model_fields.py
+++ tests/test_model_fields.py
@@ -1,6 +1,6 @@
 import pytest
 
-from flake8_django.checkers.model_fields import NOT_BLANK_TRUE_FIELDS, NOT_NULL_TRUE_FIELDS
+from flake8_django.checkers.model_fields import NOT_NULL_TRUE_FIELDS
 
 from .utils import run_check, error_code_in_result
 
@@ -17,17 +17,3 @@ def test_not_null_fields_success(field_type):
     code = "field = models.{}()".format(field_type)
     result = run_check(code)
     assert not error_code_in_result('DJ01', result)
-
-
-@pytest.mark.parametrize('field_type', NOT_BLANK_TRUE_FIELDS)
-def test_not_blank_fields_fails(field_type):
-    code = "another_field = models.{}(blank=True)".format(field_type)
-    result = run_check(code)
-    assert error_code_in_result('DJ02', result)
-
-
-@pytest.mark.parametrize('field_type', NOT_BLANK_TRUE_FIELDS)
-def test_not_blank_fields_sucess(field_type):
-    code = "another_field = models.{}()".format(field_type)
-    result = run_check(code)
-    assert not error_code_in_result('DJ02', result)
